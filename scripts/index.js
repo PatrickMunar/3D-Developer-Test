dcabfff73948bbf6b405a79ab0da099f84b0c1e1
PatrickMunar <rtmunar@up.edu.ph>
2023-04-23 20:51:55 +0800

    Replace Files

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,51 @@
 /**
- * Initial Settings
+ * Shaders
  */
-// GSAP Settings
-gsap.registerPlugin(ScrollTrigger)
+const vertexShader = `
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 modelMatrix;
+    uniform mat4 rotationMatrix;
 
-// Clear Scroll Memory
-window.history.scrollRestoration = "manual"
+    attribute vec3 position;
+    attribute vec2 uv;
 
-// Lenis Smooth Scrolling
-const lenis = new Lenis({
-  duration: 1.5,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-  direction: "vertical", // vertical, horizontal
-  gestureDirection: "vertical", // vertical, horizontal, both
-  smooth: true,
-  mouseMultiplier: 1,
-  smoothTouch: false,
-  touchMultiplier: 2,
-  infinite: false,
-})
+    varying vec2 vUv;
 
-const raf = (time) => {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
+    void main() {
+        vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+        vec4 viewPosition = viewMatrix * modelPosition;
+        vec4 projectedPosition = projectionMatrix * viewPosition;
 
-requestAnimationFrame(raf)
+        gl_Position = projectedPosition;
+
+        vUv = uv;
+    }
+`
+
+const fragmentShader = `
+    precision mediump float;
+
+    uniform sampler2D uTexture;
+    uniform vec3 uColor;
+
+    varying vec2 vUv;
+
+    void main() {
+        vec4 textureColor = texture2D(uTexture, vUv);
+        gl_FragColor = vec4(uColor.r, uColor.g, uColor.b, textureColor.a);
+    }
+`
+
+const colors = [
+  new THREE.Color(0x5f22d9),
+  new THREE.Color(0xffc1cc),
+  new THREE.Color(0x87ceeb),
+  new THREE.Color(0xbab86c),
+  new THREE.Color(0xfa5f55),
+  new THREE.Color(0xf0f0f0),
+  new THREE.Color(0x303030),
+]
 
 /**
  * Main JS
@@ -51,61 +71,126 @@ const main = () => {
     height: window.innerHeight,
   }
 
-  let cameraMaxY = -20
-
-  // Responsive Variable Changes
-  if (sizes.width > sizes.height) {
-    cameraMaxY = -20
-  } else {
-    cameraMaxY = -50
-  }
-
-  let prevHeight = sizes.height
-
   // Resize
   window.addEventListener("resize", () => {
-    if (window.innerHeight >= prevHeight) {
-      // Update sizes
-      sizes.width = window.innerWidth
-      sizes.height = window.innerHeight
-      prevHeight = sizes.height
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-      // Update camera
-      camera.aspect = sizes.width / sizes.height
-      camera.position.z = (7 * (1920 / 1080)) / (sizes.width / sizes.height)
-      camera.updateProjectionMatrix()
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-      // Update renderer
-      renderer.setSize(sizes.width, sizes.height)
-      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    }
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   })
 
   // Texture Loader
   const textureLoader = new THREE.TextureLoader()
-  const cubeTextureLoader = new THREE.CubeTextureLoader()
+  const texture = textureLoader.load("./images/Logo.png")
 
   // GLTF Loader
+  const bottle = new THREE.Group()
+  const base = new THREE.Group()
+  const cap = new THREE.Group()
+  const hinge = new THREE.Group()
+  const sleeve = new THREE.Group()
+  const modelScale = 0.05
   const gltfLoader = new THREE.GLTFLoader()
-  //   gltfLoader.load("./glb/Logo.glb", (obj) => {
-  //     rLogo3D.add(obj.scene)
-  //     obj.scene.scale.set(logoScale, logoScale, logoScale)
 
-  //     obj.scene.children[0].material = new THREE.MeshStandardMaterial({
-  //       color: new THREE.Color(0xffffff),
-  //       emissive: new THREE.Color(0xeb5f28),
-  //       emissiveIntensity: 0.1,
-  //       envMap: envMapTexture,
-  //       roughness: 0.1,
-  //       metalness: 0.1,
-  //     })
-  //   })
+  // Base
+  gltfLoader.load("./glb/Base.glb", (obj) => {
+    base.add(obj.scene)
+    obj.scene.scale.set(modelScale, modelScale, modelScale)
+
+    obj.scene.children[0].material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(0xf0f0f0),
+      roughness: 0,
+    })
+  })
+  bottle.add(base)
+
+  // Cap
+  gltfLoader.load("./glb/Cap.glb", (obj) => {
+    cap.add(obj.scene)
+    obj.scene.scale.set(modelScale, modelScale, modelScale)
+
+    obj.scene.children[0].material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(0x5f22d9),
+      roughness: 1,
+    })
+  })
+  bottle.add(cap)
+
+  // Hinge
+  gltfLoader.load("./glb/Hinge.glb", (obj) => {
+    hinge.add(obj.scene)
+    obj.scene.scale.set(modelScale, modelScale, modelScale)
+
+    obj.scene.children[0].material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(0xf0f0f0),
+      roughness: 0,
+    })
+  })
+  cap.add(hinge)
+
+  // Sleeve
+  gltfLoader.load("./glb/Sleeve.glb", (obj) => {
+    sleeve.add(obj.scene)
+    obj.scene.scale.set(modelScale, modelScale, modelScale)
+
+    obj.scene.children[0].material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(0xfa5f55),
+      roughness: 1,
+    })
+  })
+  bottle.add(sleeve)
+
+  bottle.position.y = -2
+  bottle.rotation.y = Math.PI / 2
+
+  const bottleGroup = new THREE.Group()
+  bottleGroup.add(bottle)
+  scene.add(bottleGroup)
+
+  bottleGroup.rotation.x = Math.PI / 2
 
   /**
    * 3D Objects
    */
   // ----------------------------------------------------------------
+  const logoImage = document.querySelector(".logoImage")
+  const logoPlaneGeometry = new THREE.PlaneGeometry(
+    0.006 * logoImage.naturalWidth,
+    0.006 * logoImage.naturalHeight,
+    10,
+    10
+  )
 
+  const logoPlaneMaterial = new THREE.RawShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms: {
+      uTexture: { value: texture },
+      uColor: { value: colors[3] },
+    },
+    transparent: true,
+  })
+
+  const logoPlane = new THREE.Mesh(logoPlaneGeometry, logoPlaneMaterial)
+  bottleGroup.add(logoPlane)
+  logoPlane.position.z = 0.705
+
+  for (let i = 0; i < 121; i++) {
+    const x = i * 3
+    const xVal = logoPlane.geometry.attributes.position.array[x]
+    const z = i * 3 + 2
+    const d = 0.705 - (0.705 ** 2 - xVal ** 2) ** 0.5
+    logoPlane.geometry.attributes.position.array[z] = -d
+  }
+
+  //
   /**
    * Renderer Setup
    */
@@ -118,14 +203,11 @@ const main = () => {
     0.1,
     100
   )
-  camera.position.set(0, 0, (10 * (1920 / 1080)) / (sizes.width / sizes.height))
+  camera.position.set(0, 0, 7)
   scene.add(camera)
   camera.add(ambientLight)
   camera.add(directionalLight)
   directionalLight.position.set(10, 10, 10)
-
-  // Controls
-  // const controls = new THREE.OrbitControls(camera, canvas)
 
   // Renderer
   const renderer = new THREE.WebGLRenderer({
@@ -137,6 +219,14 @@ const main = () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   renderer.shadowMap.enabled = false
   renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+  // Controls
+  const controls = new THREE.OrbitControls(camera, canvas)
+  controls.enabled = false
+  controls.enableDamping = true
+  controls.enablePan = false
+  controls.enableZoom = false
+  controls.update()
 
   /**
    * Events
@@ -151,10 +241,6 @@ const main = () => {
     x: 0,
     y: 0,
   }
-
-  let prevX = 0
-  let prevY = 0
-  let deltaXY = 0
 
   // Mouse Event Listeners Function
   const pointerMoveEvents = () => {
@@ -207,6 +293,189 @@ const main = () => {
 
   pointerMoveEvents()
 
+  // Startup Animation
+  gsap.to(bottleGroup.rotation, {
+    duration: 1,
+    delay: 3.5,
+    x: 0,
+    ease: "elastic",
+  })
+  setTimeout(() => {
+    controls.enabled = true
+  }, 4500)
+
+  // Color Choice Events
+  const changeColor = (index, color) => {
+    if (index == 0) {
+      cap.children[1].children[0].material.color = colors[color]
+    }
+    if (index == 1) {
+      cap.children[0].children[0].children[0].material.color = colors[color]
+      base.children[0].children[0].material.color = colors[color]
+    }
+    if (index == 2) {
+      sleeve.children[0].children[0].material.color = colors[color]
+    }
+    if (index == 3) {
+      logoPlane.material.uniforms.uColor.value = colors[color]
+    }
+  }
+
+  const choiceIndex = [0, 5, 4, 3]
+
+  const colorChoiceCaps = document.querySelectorAll(".colorChoiceCap")
+  const colorChoiceBases = document.querySelectorAll(".colorChoiceBase")
+  const colorChoiceSleeves = document.querySelectorAll(".colorChoiceSleeve")
+  const colorChoiceLogos = document.querySelectorAll(".colorChoiceLogo")
+
+  for (let i = 0; i < colorChoiceCaps.length; i++) {
+    if (i == choiceIndex[0]) {
+      gsap.to(colorChoiceCaps[i], { duration: 0, scale: 1.3 })
+    }
+
+    colorChoiceCaps[i].addEventListener("click", () => {
+      if (i != choiceIndex[0]) {
+        gsap.to(colorChoiceCaps[i], { duration: 0.2, y: "0vw" })
+
+        gsap.to(colorChoiceCaps[choiceIndex[0]], {
+          duration: 0.2,
+          scale: 1,
+          ease: "back",
+        })
+        gsap.to(colorChoiceCaps[i], { duration: 0.2, scale: 1.3, ease: "back" })
+        choiceIndex[0] = i
+
+        changeColor(0, choiceIndex[0])
+      }
+    })
+
+    colorChoiceCaps[i].addEventListener("pointerover", () => {
+      if (i != choiceIndex[0]) {
+        gsap.to(colorChoiceCaps[i], { duration: 0.2, y: "-0.25vw" })
+      }
+    })
+
+    colorChoiceCaps[i].addEventListener("pointerout", () => {
+      if (i != choiceIndex[0]) {
+        gsap.to(colorChoiceCaps[i], { duration: 0.2, y: "0vw" })
+      }
+    })
+  }
+
+  for (let i = 0; i < colorChoiceBases.length; i++) {
+    if (i == choiceIndex[1]) {
+      gsap.to(colorChoiceBases[i], { duration: 0, scale: 1.3 })
+    }
+
+    colorChoiceBases[i].addEventListener("click", () => {
+      if (i != choiceIndex[1]) {
+        gsap.to(colorChoiceBases[i], { duration: 0.2, y: "0vw" })
+
+        gsap.to(colorChoiceBases[choiceIndex[1]], {
+          duration: 0.2,
+          scale: 1,
+          ease: "back",
+        })
+        gsap.to(colorChoiceBases[i], {
+          duration: 0.2,
+          scale: 1.3,
+          ease: "back",
+        })
+        choiceIndex[1] = i
+
+        changeColor(1, choiceIndex[1])
+      }
+    })
+
+    colorChoiceBases[i].addEventListener("pointerover", () => {
+      if (i != choiceIndex[1]) {
+        gsap.to(colorChoiceBases[i], { duration: 0.2, y: "-0.25vw" })
+      }
+    })
+
+    colorChoiceBases[i].addEventListener("pointerout", () => {
+      if (i != choiceIndex[1]) {
+        gsap.to(colorChoiceBases[i], { duration: 0.2, y: "0vw" })
+      }
+    })
+  }
+
+  for (let i = 0; i < colorChoiceSleeves.length; i++) {
+    if (i == choiceIndex[2]) {
+      gsap.to(colorChoiceSleeves[i], { duration: 0, scale: 1.3 })
+    }
+
+    colorChoiceSleeves[i].addEventListener("click", () => {
+      if (i != choiceIndex[2]) {
+        gsap.to(colorChoiceSleeves[i], { duration: 0.2, y: "0vw" })
+
+        gsap.to(colorChoiceSleeves[choiceIndex[2]], {
+          duration: 0.2,
+          scale: 1,
+          ease: "back",
+        })
+        gsap.to(colorChoiceSleeves[i], {
+          duration: 0.2,
+          scale: 1.3,
+          ease: "back",
+        })
+        choiceIndex[2] = i
+
+        changeColor(2, choiceIndex[2])
+      }
+    })
+
+    colorChoiceSleeves[i].addEventListener("pointerover", () => {
+      if (i != choiceIndex[2]) {
+        gsap.to(colorChoiceSleeves[i], { duration: 0.2, y: "-0.25vw" })
+      }
+    })
+
+    colorChoiceSleeves[i].addEventListener("pointerout", () => {
+      if (i != choiceIndex[2]) {
+        gsap.to(colorChoiceSleeves[i], { duration: 0.2, y: "0vw" })
+      }
+    })
+  }
+
+  for (let i = 0; i < colorChoiceLogos.length; i++) {
+    if (i == choiceIndex[3]) {
+      gsap.to(colorChoiceLogos[i], { duration: 0, scale: 1.3 })
+    }
+
+    colorChoiceLogos[i].addEventListener("click", () => {
+      if (i != choiceIndex[3]) {
+        gsap.to(colorChoiceLogos[i], { duration: 0.2, y: "0vw" })
+
+        gsap.to(colorChoiceLogos[choiceIndex[3]], {
+          duration: 0.2,
+          scale: 1,
+          ease: "back",
+        })
+        gsap.to(colorChoiceLogos[i], {
+          duration: 0.2,
+          scale: 1.3,
+          ease: "back",
+        })
+        choiceIndex[3] = i
+
+        changeColor(3, choiceIndex[3])
+      }
+    })
+
+    colorChoiceLogos[i].addEventListener("pointerover", () => {
+      if (i != choiceIndex[3]) {
+        gsap.to(colorChoiceLogos[i], { duration: 0.2, y: "-0.25vw" })
+      }
+    })
+
+    colorChoiceLogos[i].addEventListener("pointerout", () => {
+      if (i != choiceIndex[3]) {
+        gsap.to(colorChoiceLogos[i], { duration: 0.2, y: "0vw" })
+      }
+    })
+  }
+
   /**
    * Animate
    */
@@ -220,6 +489,8 @@ const main = () => {
     // Render
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
+
+    controls.update()
   }
 
   tick()
@@ -257,6 +528,13 @@ const main = () => {
  * Load
  */
 window.addEventListener("load", () => {
-  gsap.to(".loadingPage", { duration: 1, delay: 1, opacity: 0 })
+  gsap.to(".loadingImage", {
+    duration: 3,
+    delay: 0,
+    rotationZ: "720deg",
+    ease: "back",
+  })
+  gsap.to(".loadingPage", { duration: 0, delay: 3, opacity: 0 })
+
   main()
 })
